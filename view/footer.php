9413f26aed2cca6e99ae72534387540968d801a5
Tryden12 <64554065+Tryden12@users.noreply.github.com>
2022-04-14 18:40:24 -0500

    initial commit

---
+++ view/footer.php
@@ -0,0 +1,16 @@
+<footer class="text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <p>Copyright © OverStock 2022. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../../../3350-User-Design/Assignments/ryden_tyler_assignment4/js/jquery-3.4.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../../../3350-User-Design/Assignments/ryden_tyler_assignment4/js/popper.min.js"></script>
+    <script src="../../../3350-User-Design/Assignments/ryden_tyler_assignment4/js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
