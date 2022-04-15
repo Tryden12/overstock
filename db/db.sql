35d889091f3e5992690af2e8db1dcc659c5ed89f
Tyler Ryden <tryden11@gmail.com>
2022-04-15 09:34:43 -0500

    cleanse

--- db/db.sql
+++
@@ -1,8 +0,0 @@
--- created this file just for template purposes
--- feel free to delete
-
--- create and select the database
-DROP DATABASE IF EXISTS -- (plug db name)
-CREATE DATABASE -- (plug db name)
-USE -- (plug db name)
-
