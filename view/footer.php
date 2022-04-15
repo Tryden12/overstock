78605c7ae8859cf3338f86c9377dfaf9f9704cec
Tyler Ryden <tryden11@gmail.com>
2022-04-15 09:38:21 -0500

    imported all files back

---
+++ view/footer.php
@@ -0,0 +1,37 @@
+<!-- Footer -->
+<div class="container text-white bg-dark p-4">
+      <div class="row">
+        <div class="col-6 col-md-8 col-lg-7">
+          <div class="row">
+            <a class="navbar-brand" href="#"><img class="w-25" src="images/overstock_logo_tp_white.png"></a>
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-5 col-6">
+          <address>
+            <strong>OverStock, Inc.</strong><br>
+            Clarksville, TN, 99110-0219<br>
+            <abbr title="Phone"></abbr> (123) 456-7890
+          </address>
+          <address>
+            <strong>Contact Us:</strong><br>
+            <a href="mailto:#">support@overstock.com</a>
+          </address>
+        </div>
+      </div>
+    </div>
+    <footer class="text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <p>Copyright © OverStock 2022. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="bootstrap_dist/js/jquery-3.4.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap_dist/js/popper.min.js"></script>
+    <script src="bootstrap_dist/js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
