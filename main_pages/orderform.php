ac73f688092ddb70ec268dfaac1aa6c8ab04509f
Tyler Ryden <tryden11@gmail.com>
2022-04-15 17:10:31 -0500

    updated orderform

--- main_pages/orderform.php
+++ main_pages/orderform.php
@@ -30,12 +30,18 @@
                             <tr>
                                 <td>
                                     <figure class="media">
-                                        <div class="img-wrap"><img src="images/items/1.jpg" class="img-thumbnail img-sm"></div>
+                                        <div class="img-wrap">
+                                            
+                                            <!--========  PHP CODE HERE  ========-->
+                                            <img class="w-50" src="../images/mensboot.png" class="img-thumbnail img-sm">
+                                        </div>
+
+
                                             <figcaption class="media-body">
 
 
                                                 <!--========  PHP CODE HERE  ========-->
-                                                <h6 class="title text-truncate">Product name goes here</h6>
+                                                <h6 class="title text-truncate pt-2">Product name goes here</h6>
 
 
                                             </figcaption>
@@ -61,12 +67,18 @@
                             <tr>
                                 <td>
                                     <figure class="media">
-                                        <div class="img-wrap"><img src="images/items/1.jpg" class="img-thumbnail img-sm"></div>
+                                    <div class="img-wrap">
+                                            
+                                            <!--========  PHP CODE HERE  ========-->
+                                            <img class="w-50" src="../images/mensboot.png" class="img-thumbnail img-sm">
+                                        </div>
+
+
                                             <figcaption class="media-body">
 
 
                                                 <!--========  PHP CODE HERE  ========-->
-                                                <h6 class="title text-truncate">Product name goes here</h6>
+                                                <h6 class="title text-truncate pt-2">Product name goes here</h6>
 
 
                                             </figcaption>
@@ -92,12 +104,18 @@
                             <tr>
                                 <td>
                                     <figure class="media">
-                                        <div class="img-wrap"><img src="images/items/1.jpg" class="img-thumbnail img-sm"></div>
+                                    <div class="img-wrap">
+                                            
+                                            <!--========  PHP CODE HERE  ========-->
+                                            <img class="w-50" src="../images/mensboot.png" class="img-thumbnail img-sm">
+                                        </div>
+
+
                                             <figcaption class="media-body">
 
 
                                                 <!--========  PHP CODE HERE  ========-->
-                                                <h6 class="title text-truncate">Product name goes here</h6>
+                                                <h6 class="title text-truncate pt-2">Product name goes here</h6>
 
 
                                             </figcaption>
@@ -125,11 +143,11 @@
                     <!--======== END OF TABLE ========-->
                 </div> 
                 <!--======================================== END of shopping cart edit section ================================ -->
-            </main> <!-- col.// -->
+            </main> <!-- END of col -->
             <aside class="col-sm-3">
                 <dl class="dlist-align">
                 <dt>Subtotal </dt>
-                <dd class="text-right">USD $568</dd>
+                <dd class="text-right">USD $75.00</dd>
                 </dl>
                 <dl class="dlist-align">
                 <dt>Shipping:</dt>
@@ -141,17 +159,23 @@
                 </dl>
                 <dl class="dlist-align h4">
                 <dt>Total:</dt>
-                <dd class="text-right"><strong>USD $1,650</strong></dd>
+                <dd class="text-right"><strong>USD $87.56</strong></dd>
                 </dl>
                 <hr>
                 <figure class="itemside mb-3">
-                    <aside class="aside"><img src="../images/mastercard.png">
-                    <img src="../images/visa.png"></aside>
+                    <aside class="aside">
+                        <img src="../images/mastercard.png">
+                        <img src="../images/visa.png">
+                    </aside>
+                    <aside class="aside">
+                        <img src="../images/apple-pay.png">
+                        <img src="../images/paypal.png">
+                    </aside>
 
-            </aside> <!-- col.// -->
+            </aside> <!-- END of col -->
         </div>
 
-    </div> <!-- container .//  -->
+    </div> <!-- END of container  -->
 </section>
 <!-- ========================= SECTION CONTENT END// ========================= -->
 
