4e6e59d92d2e8c8df1a6755beaa39029de8333d4
Tyler Ryden <tryden11@gmail.com>
2022-04-15 08:07:23 -0500

    corrected favicon
    updated on index.html

--- view/header.php
+++ view/header.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Overstock</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="../image/x-icon" href="images/favicon.ico"/>
     <!-- Bootstrap -->
     <link href="bootstrap_dist/css/bootstrap.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet" type="text/css">
