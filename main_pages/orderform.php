cdf0d17b059d7e02ef948d1790cf2457940e4ce5
Tyler Ryden <tryden11@gmail.com>
2022-04-18 13:02:31 -0500

    updated checkout form

--- main_pages/orderform.php
+++ main_pages/orderform.php
@@ -145,33 +145,48 @@
                 <!--======================================== END of shopping cart edit section ================================ -->
             </main> <!-- END of col -->
             <aside class="col-sm-3">
-                <dl class="dlist-align">
-                <dt>Subtotal </dt>
-                <dd class="text-right">USD $75.00</dd>
-                </dl>
-                <dl class="dlist-align">
-                <dt>Shipping:</dt>
-                <dd class="text-right">FREE</dd>
-                </dl>
-                <dl class="dlist-align">
-                <dt>Tax (7%):</dt>
-                <dd class="text-right">$12.56</dd>
-                </dl>
-                <dl class="dlist-align h4">
-                <dt>Total:</dt>
-                <dd class="text-right"><strong>USD $87.56</strong></dd>
-                </dl>
-                <hr>
-                <figure class="itemside mb-3">
+                
+                    <!-- Order Totals Calculation -->
+                <div class="row">
+                    <div">
+                        <h4 class="d-flex justify-content-between align-items-center mb-3">
+                            <span class="text-muted">Totals</span>
+                        </h4>
+                        <ul class="list-group mb-3">
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">Subtotal:</h6>
+                                </div>
+                                <span class="text-muted">$75.00</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">Shipping:</h6>
+                                    <small class="text-muted">FREE</small>
+                                </div>
+                                <span class="text-muted">$0.00</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <div>
+                                    <h6 class="my-0">Tax</h6>
+                                    <small class="text-muted">7%</small>
+                                </div>
+                                <span class="text-muted">$9.12</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between">
+                                <h5 class="my-0">Total (USD)</h5>
+                                <strong>$20</strong>
+                            </li>
+                        </ul>
+                        <figure class="itemside mb-3">
                     <aside class="aside">
                         <img src="../images/mastercard.png">
                         <img src="../images/visa.png">
-                    </aside>
-                    <aside class="aside">
-                        <img src="../images/apple-pay.png">
                         <img src="../images/paypal.png">
                     </aside>
+                </figure>
 
+                    </div>
             </aside> <!-- END of col -->
         </div>
 
@@ -189,49 +204,6 @@
                     <h2 class="my-3">Checkout form</h2>
                 </div>
 
-                <!-- Order Totals Calculation -->
-                <div class="row py-5">
-                    <div class="col-md-4 order-md-2 mb-4">
-                        <h4 class="d-flex justify-content-between align-items-center mb-3">
-                            <span class="text-muted">Your cart</span>
-                            <span class="badge badge-secondary badge-pill">3</span>
-                        </h4>
-                        <ul class="list-group mb-3">
-                            <li class="list-group-item d-flex justify-content-between lh-condensed">
-                                <div>
-                                    <h6 class="my-0">Product name</h6>
-                                    <small class="text-muted">Brief description</small>
-                                </div>
-                                <span class="text-muted">$12</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-condensed">
-                                <div>
-                                    <h6 class="my-0">Second product</h6>
-                                    <small class="text-muted">Brief description</small>
-                                </div>
-                                <span class="text-muted">$8</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-condensed">
-                                <div>
-                                    <h6 class="my-0">Third item</h6>
-                                    <small class="text-muted">Brief description</small>
-                                </div>
-                                <span class="text-muted">$5</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between bg-light">
-                                <div class="text-success">
-                                    <h6 class="my-0">Promo code</h6>
-                                    <small>EXAMPLE CODE</small>
-                                </div>
-                                <span class="text-success">-$5</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between">
-                                <span>Total (USD)</span>
-                                <strong>$20</strong>
-                            </li>
-                        </ul>
-
-                    </div>
 
                     <!-- Checkout form begins -->
                     <div class="col-md-8 order-md-1">
