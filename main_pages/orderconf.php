f64e1fd7fd28374bdb115995e1ab089e486aff61
Tyler Ryden <tryden11@gmail.com>
2022-04-18 13:51:32 -0500

    created confirmation page

--- main_pages/orderconf.php
+++ main_pages/orderconf.php
@@ -5,17 +5,16 @@
 
 <!-- Content goes below -->
 
+<section>
+<div class="jumbotron text-center p-5">
+  <h1 class="display-3 mt-5">Thank You!</h1>
+  <p class="lead pt-2">Your order has been <strong>confirmed!</strong> You should receive your package(s) within the next 5-7 business days.</p>
 
-
-<div class="placeholder-div">Placeholder (DELETE THIS DIV)</div>
-
-
-
-
-
-
-
-
+  <p class="p-5">
+    <a class="btn btn-primary btn-sm" href="../index.php" role="button">Continue to homepage</a>
+  </p>
+</div>
+</section>
 
 
 <!-- End of content -->
