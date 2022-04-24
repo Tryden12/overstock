aaaddb52af289ab26b44716873b3cd771aa2bd61
Tyler Ryden <tryden11@gmail.com>
2022-04-24 07:57:52 -0500

    2 errors when loading
    lines 11 and 12 set off errors. I'm not quite sure how to fix at the moment

--- main_pages/uploadimg.php
+++ main_pages/uploadimg.php
@@ -10,24 +10,22 @@ $msg = "";
   
     $filename = $_FILES["uploadImage"]["name"];
     $tempname = $_FILES["uploadImage"]["tmp_name"];    
-        $folder = "../images/".$filename;
-          
-    $db = mysqli_connect("localhost", "root", "", "photos");
-  
+    $folder   = "../images/".$filename;
+            
         // Get all the submitted data from the form
-        $sql = "INSERT INTO image (filename) VALUES ('$filename')";
+        $sql1 = "INSERT INTO images (filename) VALUES ('$filename')";
   
         // Execute query
-        mysqli_query($db, $sql);
+        mysqli_query($link, $sql1);
           
         // Now let's move the uploaded image into the folder: image
         if (move_uploaded_file($tempname, $folder))  {
             $msg = "Image uploaded successfully";
         }else{
-            $msg = "Failed to upload image";
+            $msg = "Failed to upload the image";
       }
   }
-  $result = mysqli_query($db, "SELECT * FROM image");
+  $result = mysqli_query($link, "SELECT * FROM images");
 
 ?>
 
@@ -39,18 +37,22 @@ $msg = "";
 <!-- Content goes below -->
 
 
-
+<!-- Banner -->
 <div class="container">
   <div class="jumbotron mt-5 mb-3">
     <h1 class="text-center pt-3">Upload Image</h1>      
     <p class="text-center pb-3">Upload your image below to be saved in the database.</p>
-  </div>
-     
+  </div> 
 </div>
+
+
 
 <div class="container col-md-6 mb-5">
         <img id="frame" src="" class="img-fluid p-3" />
             <div class="mb-5">
+
+
+
                 <!--====== FORM START ======-->
                 <form method="POST" action="" enctype="multipart/form-data">
 
@@ -59,13 +61,20 @@ $msg = "";
                     <div class="d-grid gap-2 col-3 mx-auto">
                         <button type="submit" name="upload" class="btn btn-primary mt-3" onclick="clearImage()">Upload</button>
                     </div>
-
-
+                    
                 </form>
                 <!--====== FORM END ======-->
+
+
+
             </div>
         </div>
 
+
+
+
+
+        <!--========== JavaScript to Preview the Image ==========-->
         <script>
             function previewImage() {
                 frame.src = URL.createObjectURL(event.target.files[0]);
