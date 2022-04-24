8185138c318bf2dd99947ed921ba2e40efefbbc3
Tyler Ryden <tryden11@gmail.com>
2022-04-24 07:30:20 -0500

    initial attempt to create img upload

--- main_pages/uploadimg.php
+++ main_pages/uploadimg.php
@@ -8,14 +8,32 @@
 
 
 <div class="container">
-  <div class="jumbotron mt-3 mb-3">
+  <div class="jumbotron mt-5 mb-3">
     <h1 class="text-center pt-3">Upload Image</h1>      
     <p class="text-center pb-3">Upload your image below to be saved in the database.</p>
   </div>
      
 </div>
 
+<div class="container col-md-6 mb-5">
+        <img id="frame" src="" class="img-fluid p-3" />
+            <div class="mb-5">
+                <input class="form-control" type="file" id="formFile" onchange="previewImage()">
+                <div class="d-grid gap-2 col-3 mx-auto">
+                    <button onclick="clearImage()" class="btn btn-primary mt-3">Upload</button>
+                </div>
+            </div>
+        </div>
 
+        <script>
+            function previewImage() {
+                frame.src = URL.createObjectURL(event.target.files[0]);
+            }
+            function clearImage() {
+                document.getElementById('formFile').value = null;
+                frame.src = "";
+            }
+        </script>
 
 
 
