5e7fcaede0ef1495069c73b72f8140be39eca639
Tyler Ryden <tryden11@gmail.com>
2022-04-30 13:06:07 -0500

    Excess code removed

--- main_pages/orderform.php
+++ main_pages/orderform.php
@@ -150,7 +150,7 @@
 
                     
                     <div class="col-md-8 order-md-1 my-auto mb-5">
-                        <h4 class="mb-3">Billing address</h4>
+                        <h4 class="mb-3">Shipping address</h4>
 
 
                         <!-- ========== FORM BEGINS  ==========-->
@@ -223,65 +223,13 @@
                                     </div>
                                 </div>
                             </div>
-                            <hr class="mb-4">
-                            <div class="custom-control custom-checkbox p-2">
-                                <input type="checkbox" class="custom-control-input" id="same-address">
-                                <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-                            </div>
+ 
 
-                            <hr class="mb-4">
 
-                            <h4 class="mb-3">Payment</h4>
-
-                            <div class="d-block my-3">
-                                <div class="custom-control custom-radio">
-                                    <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
-                                    <label class="custom-control-label" for="credit">Credit card</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required>
-                                    <label class="custom-control-label" for="debit">Debit card</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
-                                    <label class="custom-control-label" for="paypal">Paypal</label>
-                                </div>
+                            
                             </div>
-                            <div class="row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="cc-name">Name on card</label>
-                                    <input type="text" class="form-control" id="cc-name" placeholder="" required>
-                                    <small class="text-muted">Full name as displayed on card</small>
-                                    <div class="invalid-feedback">
-                                        Name on card is required
-                                    </div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="cc-number">Credit card number</label>
-                                    <input type="text" class="form-control" id="cc-number" placeholder="" required>
-                                    <div class="invalid-feedback">
-                                        Credit card number is required
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-3 mb-3">
-                                    <label for="cc-expiration">Expiration</label>
-                                    <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-                                    <div class="invalid-feedback">
-                                        Expiration date required
-                                    </div>
-                                </div>
-                                <div class="col-md-3 mb-3">
-                                    <label for="cc-expiration">CVV</label>
-                                    <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                                    <div class="invalid-feedback">
-                                        Security code required
-                                    </div>
-                                </div>
-                            </div>
-                            <hr class="mb-4">
-                            <button class="btn btn-primary btn-lg btn-block" type="submit">Place Order</button>
+                            
+                            <button class="btn btn-primary btn-lg btn-block mb-5" type="submit">Place Order</button>
                         </form>
                     </div>
                 </div>
